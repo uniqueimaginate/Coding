232c49ff9038c6a51eb12bf5234503b016b8ac9f
uniqueimaginate <uniqueimaginate@gmail.com>
2020-07-25 11:02:36 +0900

    Algorithm_10

--- Algorithm/rmq.cpp
+++ Algorithm/rmq.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-#define INT_MAX __INT_MAX__
 
 using namespace std;
 
 // const int INT_MAX = numeric_limits<int>::max();
+// const int INT_MIN = numeric_limits<int>::min();
 
 struct RMQ{
 public:
@@ -28,6 +28,10 @@ public:
         return query(left, right, 1, 0, n-1);
     }
 
+    int update(int index, int newValue){
+        return update(index, newValue, 1, 0, n-1);
+    }
+
 private:
     int query(int left, int right, int node, int nodeLeft, int nodeRight){
         if(right < nodeLeft || nodeRight < left) return INT_MAX;
@@ -38,19 +42,15 @@ private:
         return min(query(left, right, node * 2, nodeLeft, mid),
                 query(left, right, node * 2 + 1, mid + 1, nodeRight));
     }
+
+    int update(int index, int newValue, int node, int nodeLeft, int nodeRight){
+        if(index < nodeLeft || nodeRight < index)
+            return rangeMin[node];
+        if(nodeLeft == nodeRight) return rangeMin[node] = newValue;
+        int mid = (nodeLeft + nodeRight) / 2;
+        return rangeMin[node] = min(update(index, newValue, node*2, nodeLeft, mid),
+                                update(index, newValue, node*2+1, mid+1, nodeRight));
+    }
 };
 
-vector<int> vec;
-int main(){
-    while(1){
-        int n; cin >> n;
-        if(n == 0) break;
-        while(n--){
-            int temp; cin >> temp;
-            vec.push_back(temp);
-        
-        }
-        RMQ rmq(vec);
-        cout << rmq.query(6,7) << endl;
-    }
-}
+// 이러한 형태를 이제 각 문제에 맞게 변형을 해야 한다.
