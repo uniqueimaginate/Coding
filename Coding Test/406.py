0d7ccc4364b0ab176aeb678279cf337418946f14
uniqueimaginate <uniqueimaginate@gmail.com>
2021-03-06 15:07:09 +0900

    Sliding Window, Greedy Algorithm

---
+++ Coding Test/406.py
@@ -0,0 +1,20 @@
+import re
+import collections
+import itertools
+import heapq
+import bisect
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        result = []
+        heap = []
+        
+        for person in people:
+            heapq.heappush(heap, (-person[0], person[1]))
+            
+        
+        while heap:
+            person = heapq.heappop(heap)
+            result.insert(person[1], (-person[0], person[1]))
+            
+        return result
